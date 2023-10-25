fce8845126ee97e820c2e6939061811cc405fd66
River Hansen <riverbh01@gmail.com>
2023-10-25 16:17:37 -0600

    added the server and have it set up with more packages installed

--- src/client/components/contact.jsx
+++ src/client/components/contact.jsx
@@ -4,6 +4,24 @@ const Contact = () => {
     return (
         <section>
             <h1>Contact Will for future porjects</h1>
+            <form action="/submit" method="post">
+                <label for="firstName">First Name:</label>
+                <input type="text" id="firstName" name="firstName" required />
+                
+                <label for="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="lastName" required />
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+
+                <label for="subject">Subject:</label>
+                <input type="text" id="subject" name="subject" required />
+
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+
+                <input type="submit" value="Submit" />
+            </form>
         </section>
     )
 }
